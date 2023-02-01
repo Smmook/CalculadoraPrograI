16626c7d32a7bff3e31840cde0b54208a83a1af3
Smmook <diegobix97@gmail.com>
2023-02-01 12:24:09 +0100

    Terminado primer intento de logica

---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculadora con funciones Lambda
+
+Trabajo para la universidad que consiste en hacer una calculadora por consola usando funciones lambda y composición de funciones.
--- main.cpp
+++ main.cpp
@@ -3,7 +3,26 @@
 
 using namespace std;
 
-float operate(float a, float b, const function<float (float, float)> &op);
+float operate(float a, float b, const function<float (float, float)> &op) {
+    return op(a, b);
+}
+
+auto sumar = [](float a, float b) -> float {
+    return a+b;
+};
+
+auto restar = [](float a, float b) -> float {
+    return a-b;
+};
+
+auto multiplicar = [](float a, float b) -> float {
+    return a*b;
+};
+
+auto dividir = [](float a, float b) -> float {
+    if (b == 0.f) throw domain_error("No se puede dividir por 0");
+    return a/b;
+};
 
 int main()
 {
@@ -17,43 +36,43 @@ int main()
     int sel;
 
     do {
-        cout << menu << endl;
-        cin >> sel;
+        do {
+            cout << menu << endl;
+            cin >> sel;
+        } while (sel < 1 || sel > 5);
         if (sel == 5) break;
         cout << "Introduce un numero: ";
         cin >> a;
         cout << "Introduce otro numero: ";
         cin >> b;
+        float res{0.f};
         switch (sel) {
         case 1:
-
+            res = operate(a, b, sumar);
+            break;
+        case 2:
+            res = operate(a, b, restar);
+            break;
+        case 3:
+            res = operate(a, b, multiplicar);
+            break;
+        case 4:
+            try {
+                res = operate(a, b, dividir);
+            } catch(domain_error e) {
+                cout << "No se puede dividir por cero!\n";
+            }
             break;
         default:
+            cout << "Error" << endl;
             break;
         }
+
+        cout << "\nResultado: " << res << "\n\n";
 
     } while (1);
 
     return 0;
 }
 
-float operate(float a, float b, const function<float (float, float)> &op) {
-    return op(a, b);
-}
 
-auto suma = [](float a, float b) -> float {
-    return a+b;
-};
-
-auto resta = [](float a, float b) -> float {
-    return a-b;
-};
-
-auto multiplicacion = [](float a, float b) -> float {
-    return a*b;
-};
-
-auto dividir = [](float a, float b) -> float {
-    if (b == 0.f) throw domain_error("No se puede dividir por 0");
-    return a/b;
-};
